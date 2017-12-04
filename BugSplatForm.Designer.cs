8e3cc4060a42ca018b020d9cc7a327a8569837cc
jodo993 <josephdo993@yahoo.com>
2017-12-03 16:19:04 -0800

    63rd
    bug splat form

---
+++ BugSplatForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Webber_Inventory_Search_2017_2018
+{
+    partial class BugSplatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BugSplatForm));
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.descLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(297, 312);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(310, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "An unexpected problem occurred and the program needs to exit.";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(360, 52);
+            this.label3.TabIndex = 4;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(352, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Please provide a brief description of how the problem occurred. (Optional)";
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(15, 240);
+            this.descriptionTextBox.Multiline = true;
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(357, 66);
+            this.descriptionTextBox.TabIndex = 6;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(15, 201);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Name (Optional)";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LightSalmon;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = global::Webber_Inventory_Search_2017_2018.Properties.Resources.bugSplat100;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 0, 20, 0);
+            this.label1.Size = new System.Drawing.Size(385, 100);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "BUG SPLAT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.Location = new System.Drawing.Point(12, 317);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 23);
+            this.nameLabel.TabIndex = 9;
+            this.nameLabel.Visible = false;
+            // 
+            // descLabel
+            // 
+            this.descLabel.Location = new System.Drawing.Point(53, 317);
+            this.descLabel.Name = "descLabel";
+            this.descLabel.Size = new System.Drawing.Size(42, 23);
+            this.descLabel.TabIndex = 10;
+            this.descLabel.Visible = false;
+            // 
+            // BugSplatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 344);
+            this.Controls.Add(this.descLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.closeButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BugSplatForm";
+            this.Text = "BugSplatForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label descLabel;
+    }
+}
---
+++ BugSplatForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Webber_Inventory_Search_2017_2018
+{
+    public partial class BugSplatForm : Form
+    {
+        // Use by this form only, global
+        private OleDbConnection connection = new OleDbConnection();
+
+        public BugSplatForm(string pg,string ex)
+        {
+            InitializeComponent();
+            nameLabel.Text = pg;
+            descLabel.Text = ex;
+
+            // Connect to database                                                       
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\josep\Desktop\WebberMainDatabase.accdb;Persist Security Info=False;";
+            //connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=T:\Webber Database\WebberMainDatabase_be.accdb;Persist Security Info=False;";
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            string name = "";
+            string description = "";
+            string page = nameLabel.Text;
+            string bugInformation = descLabel.Text;
+            string status = "Open";
+
+            if (nameTextBox.Text != "")
+                name = nameTextBox.Text;
+            if (descriptionTextBox.Text != "")
+                description = descriptionTextBox.Text;
+
+            // Check to see if connection is open, close if it is
+            if (connection.State == ConnectionState.Open)
+                connection.Close();
+
+            connection.Open();
+
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = connection;
+            command.CommandText = "insert into Exception_Error_Report (Page,Error,Person,PersonDescription,Status) values('" + page + "','" + bugInformation + "','" + name + "','" + description + "','" + status + "')";
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
+            this.Close();
+        }
+    }
+}
