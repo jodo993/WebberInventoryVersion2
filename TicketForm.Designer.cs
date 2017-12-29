e918fd281df35bc0c14ecb344175aba2485bf55f
jodo993 <josephdo993@yahoo.com>
2017-12-28 18:24:24 -0800

    77th Ticket form
    - tabs in order
    - edited code to allow check for ticket number
    - changed fonts to standard

--- TicketForm.Designer.cs
+++ TicketForm.Designer.cs
@@ -50,19 +50,20 @@
             this.checkButton = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label9 = new System.Windows.Forms.Label();
+            this.ticketNumberTextBox = new System.Windows.Forms.TextBox();
             this.statusLabel = new System.Windows.Forms.Label();
             this.userLabel = new System.Windows.Forms.Label();
-            this.ticketNumberTextBox = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // submitButton
             // 
+            this.submitButton.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.submitButton.Location = new System.Drawing.Point(144, 235);
             this.submitButton.Name = "submitButton";
             this.submitButton.Size = new System.Drawing.Size(75, 23);
-            this.submitButton.TabIndex = 0;
+            this.submitButton.TabIndex = 7;
             this.submitButton.Text = "Submit";
             this.submitButton.UseVisualStyleBackColor = true;
             this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
@@ -70,102 +71,114 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(61, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.Size = new System.Drawing.Size(31, 14);
             this.label1.TabIndex = 1;
             this.label1.Text = "Staff";
             // 
             // staffTextBox
             // 
-            this.staffTextBox.Location = new System.Drawing.Point(119, 32);
+            this.staffTextBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.staffTextBox.Location = new System.Drawing.Point(98, 29);
             this.staffTextBox.Name = "staffTextBox";
-            this.staffTextBox.Size = new System.Drawing.Size(100, 20);
-            this.staffTextBox.TabIndex = 2;
+            this.staffTextBox.Size = new System.Drawing.Size(121, 22);
+            this.staffTextBox.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 96);
+            this.label2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 96);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.Size = new System.Drawing.Size(69, 14);
             this.label2.TabIndex = 3;
             this.label2.Text = "Importance";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 128);
+            this.label3.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 128);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.Size = new System.Drawing.Size(53, 14);
             this.label3.TabIndex = 4;
             this.label3.Text = "Category";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 192);
+            this.label4.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 192);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.Size = new System.Drawing.Size(69, 14);
             this.label4.TabIndex = 5;
             this.label4.Text = "Description";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 160);
+            this.label5.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(5, 160);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.Size = new System.Drawing.Size(87, 14);
             this.label5.TabIndex = 6;
             this.label5.Text = "Preferred Time";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(55, 64);
+            this.label6.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(54, 64);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(38, 14);
             this.label6.TabIndex = 7;
             this.label6.Text = "Room";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(23, 19);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(71, 13);
+            this.label7.Size = new System.Drawing.Size(89, 18);
             this.label7.TabIndex = 8;
             this.label7.Text = "Create Ticket";
             // 
             // roomTextBox
             // 
-            this.roomTextBox.Location = new System.Drawing.Point(119, 61);
+            this.roomTextBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roomTextBox.Location = new System.Drawing.Point(98, 61);
             this.roomTextBox.Name = "roomTextBox";
-            this.roomTextBox.Size = new System.Drawing.Size(100, 20);
-            this.roomTextBox.TabIndex = 9;
+            this.roomTextBox.Size = new System.Drawing.Size(121, 22);
+            this.roomTextBox.TabIndex = 2;
             // 
             // descriptionTextBox
             // 
-            this.descriptionTextBox.Location = new System.Drawing.Point(119, 189);
+            this.descriptionTextBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descriptionTextBox.Location = new System.Drawing.Point(98, 189);
             this.descriptionTextBox.Multiline = true;
             this.descriptionTextBox.Name = "descriptionTextBox";
-            this.descriptionTextBox.Size = new System.Drawing.Size(100, 40);
-            this.descriptionTextBox.TabIndex = 10;
+            this.descriptionTextBox.Size = new System.Drawing.Size(121, 40);
+            this.descriptionTextBox.TabIndex = 6;
             // 
             // importanceComboBox
             // 
+            this.importanceComboBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.importanceComboBox.FormattingEnabled = true;
             this.importanceComboBox.Items.AddRange(new object[] {
             "Low - Inconvenient",
             "Medium - Moderate impairment",
             "High - Urgent fix(es) required"});
-            this.importanceComboBox.Location = new System.Drawing.Point(119, 93);
+            this.importanceComboBox.Location = new System.Drawing.Point(98, 93);
             this.importanceComboBox.Name = "importanceComboBox";
-            this.importanceComboBox.Size = new System.Drawing.Size(100, 21);
-            this.importanceComboBox.TabIndex = 11;
+            this.importanceComboBox.Size = new System.Drawing.Size(121, 22);
+            this.importanceComboBox.TabIndex = 3;
             // 
             // categoryComboBox
             // 
+            this.categoryComboBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.categoryComboBox.FormattingEnabled = true;
             this.categoryComboBox.Items.AddRange(new object[] {
             "Instructional Assistance",
@@ -175,13 +188,14 @@
             "Network",
             "Other",
             ""});
-            this.categoryComboBox.Location = new System.Drawing.Point(119, 125);
+            this.categoryComboBox.Location = new System.Drawing.Point(98, 125);
             this.categoryComboBox.Name = "categoryComboBox";
-            this.categoryComboBox.Size = new System.Drawing.Size(100, 21);
-            this.categoryComboBox.TabIndex = 12;
+            this.categoryComboBox.Size = new System.Drawing.Size(121, 22);
+            this.categoryComboBox.TabIndex = 4;
             // 
             // timeComboBox
             // 
+            this.timeComboBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.timeComboBox.FormattingEnabled = true;
             this.timeComboBox.Items.AddRange(new object[] {
             "Before School",
@@ -191,27 +205,29 @@
             "11-12PM",
             "1-2PM",
             "After School"});
-            this.timeComboBox.Location = new System.Drawing.Point(119, 157);
+            this.timeComboBox.Location = new System.Drawing.Point(98, 157);
             this.timeComboBox.Name = "timeComboBox";
-            this.timeComboBox.Size = new System.Drawing.Size(100, 21);
-            this.timeComboBox.TabIndex = 13;
+            this.timeComboBox.Size = new System.Drawing.Size(121, 22);
+            this.timeComboBox.TabIndex = 5;
             // 
             // clearButton
             // 
+            this.clearButton.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.clearButton.Location = new System.Drawing.Point(64, 235);
             this.clearButton.Name = "clearButton";
             this.clearButton.Size = new System.Drawing.Size(75, 23);
-            this.clearButton.TabIndex = 14;
+            this.clearButton.TabIndex = 8;
             this.clearButton.Text = "Clear";
             this.clearButton.UseVisualStyleBackColor = true;
             this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(114, 235);
+            this.exitButton.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(122, 235);
             this.exitButton.Name = "exitButton";
-            this.exitButton.Size = new System.Drawing.Size(75, 23);
-            this.exitButton.TabIndex = 15;
+            this.exitButton.Size = new System.Drawing.Size(67, 23);
+            this.exitButton.TabIndex = 12;
             this.exitButton.Text = "Exit";
             this.exitButton.UseVisualStyleBackColor = true;
             this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
@@ -232,6 +248,7 @@
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.roomTextBox);
             this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(26, 49);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(243, 264);
@@ -241,20 +258,22 @@
             // 
             // mainMenuButton
             // 
+            this.mainMenuButton.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.mainMenuButton.Location = new System.Drawing.Point(33, 235);
             this.mainMenuButton.Name = "mainMenuButton";
-            this.mainMenuButton.Size = new System.Drawing.Size(75, 23);
-            this.mainMenuButton.TabIndex = 17;
+            this.mainMenuButton.Size = new System.Drawing.Size(83, 23);
+            this.mainMenuButton.TabIndex = 11;
             this.mainMenuButton.Text = "Main Menu";
             this.mainMenuButton.UseVisualStyleBackColor = true;
             this.mainMenuButton.Click += new System.EventHandler(this.mainMenuButton_Click);
             // 
             // checkButton
             // 
+            this.checkButton.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.checkButton.Location = new System.Drawing.Point(82, 58);
             this.checkButton.Name = "checkButton";
             this.checkButton.Size = new System.Drawing.Size(53, 23);
-            this.checkButton.TabIndex = 18;
+            this.checkButton.TabIndex = 10;
             this.checkButton.Text = "Check";
             this.checkButton.UseVisualStyleBackColor = true;
             this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
@@ -268,6 +287,7 @@
             this.groupBox2.Controls.Add(this.statusLabel);
             this.groupBox2.Controls.Add(this.exitButton);
             this.groupBox2.Controls.Add(this.checkButton);
+            this.groupBox2.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(275, 49);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(220, 264);
@@ -278,11 +298,21 @@
             // label9
             // 
             this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(56, 16);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(105, 13);
+            this.label9.Size = new System.Drawing.Size(116, 14);
             this.label9.TabIndex = 22;
             this.label9.Text = "Enter Ticket Number";
+            // 
+            // ticketNumberTextBox
+            // 
+            this.ticketNumberTextBox.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ticketNumberTextBox.Location = new System.Drawing.Point(59, 32);
+            this.ticketNumberTextBox.Name = "ticketNumberTextBox";
+            this.ticketNumberTextBox.Size = new System.Drawing.Size(100, 22);
+            this.ticketNumberTextBox.TabIndex = 9;
+            this.ticketNumberTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // statusLabel
             // 
@@ -300,14 +330,6 @@
             this.userLabel.Size = new System.Drawing.Size(25, 25);
             this.userLabel.TabIndex = 21;
             // 
-            // ticketNumberTextBox
-            // 
-            this.ticketNumberTextBox.Location = new System.Drawing.Point(59, 32);
-            this.ticketNumberTextBox.Name = "ticketNumberTextBox";
-            this.ticketNumberTextBox.Size = new System.Drawing.Size(100, 20);
-            this.ticketNumberTextBox.TabIndex = 22;
-            this.ticketNumberTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
             // TicketForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -317,6 +339,7 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label7);
+            this.Font = new System.Drawing.Font("Calibri", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "TicketForm";
--- TicketForm.cs
+++ TicketForm.cs
@@ -22,25 +22,24 @@ namespace Webber_Inventory_Search_2017_2018
             InitializeComponent();
             userLabel.Text = user;
             // Connect to database                      
-            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=E:\Webber Inventory\V4\WebberMainDatabase.accdb;Persist Security Info=False;";
-            //connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\josep\Desktop\WebberMainDatabase.accdb;Persist Security Info=False;";
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\josep\Desktop\WebberMainDatabase.accdb;Persist Security Info=False;";
             //connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=T:\Webber Database\WebberMainDatabase_be.accdb;Persist Security Info=False;";
         }
 
         private void submitButton_Click(object sender, EventArgs e)
         {
-            try
-            {
-                if (staffTextBox.Text != "" && staffTextBox.Text.All(Char.IsLetter))
-                    if (roomTextBox.Text != "")
-                        if (importanceComboBox.Text != "" && importanceComboBox.Text == "Low - Inconvenient" || importanceComboBox.Text == "Medium - Moderate impairment"
-                            || importanceComboBox.Text == "High - Urgent fix(es) required")
-                            if (categoryComboBox.Text != "" && categoryComboBox.Text == "Instructional Assistance" || categoryComboBox.Text == "Hardware Issue" ||
-                                categoryComboBox.Text == "Software Issue" || categoryComboBox.Text == "Chromebook" || categoryComboBox.Text == "Network" ||
-                                categoryComboBox.Text == "Other")
-                                if (timeComboBox.Text != "" && timeComboBox.Text == "Before School" || timeComboBox.Text == "8-9AM" || timeComboBox.Text == "9-10AM" ||
-                                    timeComboBox.Text == "10-11AM" || timeComboBox.Text == "11-12PM" || timeComboBox.Text == "1-2PM" || timeComboBox.Text == "After School")
-                                    if (descriptionTextBox.Text != "")
+            if (staffTextBox.Text != "" && staffTextBox.Text.All(Char.IsLetter))
+                if (roomTextBox.Text != "")
+                    if (importanceComboBox.Text != "" && importanceComboBox.Text == "Low - Inconvenient" || importanceComboBox.Text == "Medium - Moderate impairment"
+                        || importanceComboBox.Text == "High - Urgent fix(es) required")
+                        if (categoryComboBox.Text != "" && categoryComboBox.Text == "Instructional Assistance" || categoryComboBox.Text == "Hardware Issue" ||
+                            categoryComboBox.Text == "Software Issue" || categoryComboBox.Text == "Chromebook" || categoryComboBox.Text == "Network" ||
+                            categoryComboBox.Text == "Other")
+                            if (timeComboBox.Text != "" && timeComboBox.Text == "Before School" || timeComboBox.Text == "8-9AM" || timeComboBox.Text == "9-10AM" ||
+                                timeComboBox.Text == "10-11AM" || timeComboBox.Text == "11-12PM" || timeComboBox.Text == "1-2PM" || timeComboBox.Text == "After School")
+                                if (descriptionTextBox.Text != "")
+                                {
+                                    try
                                     {
                                         // Date is automatically added
                                         string staff = staffTextBox.Text;
@@ -59,7 +58,7 @@ namespace Webber_Inventory_Search_2017_2018
                                         command.ExecuteNonQuery();
 
                                         // Create array
-                                        string[] idList = new string[1000];
+                                        string[] idList = new string[2000];
                                         int i = 0;
 
                                         // Which table to search for data
@@ -78,23 +77,30 @@ namespace Webber_Inventory_Search_2017_2018
 
                                         MessageBox.Show("Ticket was successfully submitted. Ticket number is #" + i + ". Ticket number will allow you to check on the ticket current status.");
                                     }
-                                    else
-                                        MessageBox.Show("Please enter a brief description of the problem.");
+                                    catch (Exception ex)
+                                    {
+                                        // Get and send information to bug report
+                                        string page = "Ticket";
+                                        string button = "Submit";
+                                        string exception = ex.ToString();
+                                        BugSplatForm bugSplat = new BugSplatForm(page, button, exception);
+                                        bugSplat.ShowDialog();
+
+                                        this.Close();
+                                    }
+                                }
                                 else
-                                    MessageBox.Show("Please select a valid time.");
+                                    MessageBox.Show("Please enter a brief description of the problem.");
                             else
-                                MessageBox.Show("Please select one of the given issue choices.");
+                                MessageBox.Show("Please select a valid time.");
                         else
-                            MessageBox.Show("Please select a valid urgency status.");
+                            MessageBox.Show("Please select one of the given issue choices.");
                     else
-                        MessageBox.Show("Please enter the location of the problem.");
+                        MessageBox.Show("Please select a valid urgency status.");
                 else
-                    MessageBox.Show("Please enter a valid name.");       
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Bug Splat! Please exit and try again." + ex);
-            }
+                    MessageBox.Show("Please enter the location of the problem.");
+            else
+                MessageBox.Show("Please enter a valid name.");
         }
 
         // Clear all fields
@@ -108,29 +114,7 @@ namespace Webber_Inventory_Search_2017_2018
             descriptionTextBox.Text = "";
         }
 
-        private void exitButton_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void mainMenuButton_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-
-            if (userLabel.Text == "T")
-            {
-                MainMenuForm2 mainMenu2 = new MainMenuForm2();
-                mainMenu2.ShowDialog();
-            }
-            else
-            {
-                MainMenuForm mainMenu = new MainMenuForm();
-                mainMenu.ShowDialog();
-            }
-                
-            this.Close();
-        }
-
+        // Get ticket number and find the information of the ticket
         private void checkButton_Click(object sender, EventArgs e)
         {
             string ticketNumber = ticketNumberTextBox.Text;
@@ -157,7 +141,8 @@ namespace Webber_Inventory_Search_2017_2018
                         string query = "select * from Help_Ticket";
                         command.CommandText = query;
 
-                        string[] idCheck = new string[1000];
+                        //List<string> idCheck = new List<string>();
+                        string[] idCheck = new string[2000];
                         int i = 0;
                         bool found = false;
 
@@ -186,6 +171,7 @@ namespace Webber_Inventory_Search_2017_2018
                     }
                     catch (Exception ex)
                     {
+                        // Get info for bug report
                         string page = "Ticket";
                         string button = "Check";
                         string exception = ex.ToString();
@@ -198,6 +184,31 @@ namespace Webber_Inventory_Search_2017_2018
                 else
                     MessageBox.Show("Ticket number must be digits only.");
             }
+        }
+
+        // Close the program
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Go back to main menu page depending on whether user was signed in as teacher or admin
+        private void mainMenuButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            if (userLabel.Text == "T")
+            {
+                MainMenuForm2 mainMenu2 = new MainMenuForm2();
+                mainMenu2.ShowDialog();
+            }
+            else
+            {
+                MainMenuForm mainMenu = new MainMenuForm();
+                mainMenu.ShowDialog();
+            }
+
+            this.Close();
         }
     }
 }
